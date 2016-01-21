2434b6cfe440ef96aba58d3f26049409de50243a
mike <hotmykeul@gmail.com>
2016-01-21 15:40:16 +0100

    tache 2 terminée

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 
+import org.ulco.GraphicsObject;
+
 import java.io.File;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 /**
  * Created by mdurieux abeaucou on 21/01/16.
@@ -7,22 +11,32 @@ import java.io.File;
 
 public class Main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        task2();
+
+    }
+
+    public static void task1() {
         File directory = new File("./src/org/ulco");
         File[] fList = directory.listFiles();
 
-        for(File f : fList){
+        for (File f : fList) {
             Class cl = null;
             try {
                 cl = Class.forName("org.ulco." + f.getName().split("\\.")[0]);
-                if(cl.getSuperclass().getName().equals("org.ulco.GraphicsObject")) System.out.println(cl.getName());
+                if (cl.getSuperclass().getName().equals("org.ulco.GraphicsObject")) System.out.println(cl.getName());
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
 
         }
-            System.out.println();
-
     }
 
+    public static void task2() {
+        Class cl = GraphicsObject.class;
+        for (Method m : cl.getDeclaredMethods()) {
+            if (Modifier.isAbstract(m.getModifiers()))
+                System.out.println(m.getName());
+        }
+    }
 }
